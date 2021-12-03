1e5b6464af9f8d51873c13ebe083aa5fb7aabbf2
Mathieu Moalic <mathieu.q.moalic@gmail.com>
2021-12-03 10:07:43 +0100

    version 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="adl",
-    version="0.1.4",
+    version="0.1.5",
     description="script generator for mumax3 for antidot lattice",
     author="Mathieu Moalic",
     author_email="matmoa@pm.me",
